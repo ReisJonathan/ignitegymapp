5d66a5d78bef0c69afbd5832805c298947303568
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-20 17:18:18 -0300

    Ajuste fora do curso: Utilizando KeyboardAvoidingView para scrollar input ao abrir teclado

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,9 @@
-import { ScrollView, TouchableOpacity } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import { Center, Heading, VStack } from '@gluestack-ui/themed';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -11,60 +16,68 @@ export function Profile() {
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
-      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
-        <Center mt="$6" px="$10">
-          <UserPhoto
-            source={{ uri: 'https://github.com/reisjonathan.png' }}
-            alt="Foto do usuário"
-            size="xl"
-          />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <ScrollView
+          automaticallyAdjustContentInsets={true}
+          contentContainerStyle={{ paddingBottom: 36 }}
+        >
+          <Center mt="$6" px="$10">
+            <UserPhoto
+              source={{ uri: 'https://github.com/reisjonathan.png' }}
+              alt="Foto do usuário"
+              size="xl"
+            />
 
-          <TouchableOpacity>
-            <Text
-              color="$green500"
+            <TouchableOpacity>
+              <Text
+                color="$green500"
+                fontFamily="$heading"
+                fontSize="$md"
+                mt="$2"
+                mb="$8"
+              >
+                Alterar Foto
+              </Text>
+            </TouchableOpacity>
+
+            <Center width="$full" gap="$4">
+              <Input placeholder="Nome" bg="$gray600" />
+              <Input
+                placeholder="E-mail"
+                value="jonathan@email.com"
+                bg="$gray600"
+                isReadOnly
+              />
+            </Center>
+
+            <Heading
+              alignSelf="flex-start"
               fontFamily="$heading"
+              color="$gray200"
               fontSize="$md"
-              mt="$2"
-              mb="$8"
+              mt="$12"
+              mb="$2"
             >
-              Alterar Foto
-            </Text>
-          </TouchableOpacity>
+              Alterar senha
+            </Heading>
 
-          <Center width="$full" gap="$4">
-            <Input placeholder="Nome" bg="$gray600" />
-            <Input
-              placeholder="E-mail"
-              value="jonathan@email.com"
-              bg="$gray600"
-              isReadOnly
-            />
+            <Center w="$full" gap="$4">
+              <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+              <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+              <Input
+                placeholder="Confirme a nova senha"
+                bg="$gray600"
+                secureTextEntry
+              />
+
+              <Button title="Atualizar" />
+            </Center>
           </Center>
-
-          <Heading
-            alignSelf="flex-start"
-            fontFamily="$heading"
-            color="$gray200"
-            fontSize="$md"
-            mt="$12"
-            mb="$2"
-          >
-            Alterar senha
-          </Heading>
-
-          <Center w="$full" gap="$4">
-            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
-            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
-            <Input
-              placeholder="Confirme a nova senha"
-              bg="$gray600"
-              secureTextEntry
-            />
-
-            <Button title="Atualizar" />
-          </Center>
-        </Center>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </VStack>
   );
 }
