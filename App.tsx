71d97137aa8607756a50fcde4b5af953978625da
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-19 22:38:36 -0300

    Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
+
+import { Routes } from '@routes/index';
 import { Loading } from '@components/Loading';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   );
 }
