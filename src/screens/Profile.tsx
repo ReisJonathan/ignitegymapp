e9e13f1cd994290d730535b8301071e3f7bcaa85
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-20 16:40:56 -0300

    Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,22 @@
+import { ScrollView } from 'react-native';
+import { Center, VStack } from '@gluestack-ui/themed';
 import { ScreenHeader } from '@components/ScreenHeader';
-import { VStack } from '@gluestack-ui/themed';
+import { UserPhoto } from '@components/UserPhoto';
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center>
+          <UserPhoto
+            source={{ uri: 'https://github.com/reisjonathan.png' }}
+            alt="Foto do usuário"
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
