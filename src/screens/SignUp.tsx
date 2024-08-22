274f3eaf42f8ba5aaead12dd007c6e99a7f2abde
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-21 23:44:02 -0300

    Tipando os dados dos formularios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -14,8 +14,15 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { useForm, Controller } from 'react-hook-form';
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
+
 export function SignUp() {
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm<FormDataProps>();
 
   const navigation = useNavigation();
 
@@ -23,7 +30,7 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: any) {
+  function handleSignUp(data: FormDataProps) {
     console.log(data);
   }
 
