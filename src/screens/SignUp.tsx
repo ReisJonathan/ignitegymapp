ecd6852c2d6d75e2e785fe837a23d691d861ae4e
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-22 22:48:25 -0300

    Obtendo resposta do back-end

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -63,7 +63,9 @@ export function SignUp() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
