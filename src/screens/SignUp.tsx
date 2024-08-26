a3b82cbae1d63d7fce3632c593cca974e57f3df0
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-26 17:24:11 -0300

    Criando conta e logando usuario

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   VStack,
   Image,
@@ -20,6 +21,7 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { api } from '@services/api';
 import { AppError } from '@utils/AppError';
+import { useAuth } from '@hooks/useAuth';
 
 type FormDataProps = {
   name: string;
@@ -42,6 +44,9 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const { signIn } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
   const toast = useToast();
 
   const {
@@ -64,9 +69,14 @@ export function SignUp() {
 
   async function handleSignUp(data: FormDataProps) {
     try {
+      setIsLoading(true);
+
       const response = await api.post('/users', data);
-      console.log(response.data);
+
+      await signIn(data.email, data.password);
     } catch (error) {
+      setIsLoading(false);
+
       const isAppError = error instanceof AppError;
 
       const title = isAppError
@@ -172,6 +182,7 @@ export function SignUp() {
             <Button
               title="Criar e acessar"
               onPress={handleSubmit(handleSignUp)}
+              isLoading={isLoading}
             />
           </Center>
           <Button
