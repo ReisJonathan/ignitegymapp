05ecf5fd79caab9c22cb68ab3904d6e15535f8c4
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-26 10:23:10 -0300

    Criando seu proprio hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,12 @@ import { AuthRoutes } from './auth.routes';
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config';
 import { Box } from '@gluestack-ui/themed';
-import { AppRoutes } from './app.routes';
-import { useContext } from 'react';
-import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from '@hooks/useAuth';
 
 export function Routes() {
-  const contextData = useContext(AuthContext);
-  console.log('Usuario logado => ', contextData);
+  const { user } = useAuth();
+
+  console.log('Usuario logado => ', user);
 
   const theme = DefaultTheme;
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
