56e69c049083d71f5ada989daf1000b2926c4307
Jonathan Reis <jonathanreis7834@gmail.com>
2024-08-28 17:15:18 -0300

    Exibindo exercicios do grupo

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -8,10 +8,13 @@ import {
 } from '@gluestack-ui/themed';
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import { ChevronRight } from 'lucide-react-native';
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
-type Props = TouchableOpacityProps & {};
+type Props = TouchableOpacityProps & {
+  data: ExerciseDTO;
+};
 
-export function ExerciseCard({ ...rest }: Props) {
+export function ExerciseCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity {...rest}>
       <HStack
@@ -36,10 +39,10 @@ export function ExerciseCard({ ...rest }: Props) {
 
         <VStack flex={1}>
           <Heading fontSize="$lg" color="$white" fontFamily="$heading">
-            Puxada Frontal
+            {data.name}
           </Heading>
           <Text fontSize="$sm" color="$gray200" mt="$1" numberOfLines={2}>
-            3 séries x 12 repetições
+            {data.series} séries x {data.repetitions} repetições
           </Text>
         </VStack>
 
